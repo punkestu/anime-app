808f2b89de68715f6d430ae443be2f8b5772c103
Dobberman <pangestubima89@gmail.com>
2024-01-19 01:24:50 +0700

    create complete page

--- src/apps/anime/webController.ts
+++ src/apps/anime/webController.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from "express";
-import { getHomeAnime, searchAnime } from "./repo";
+import {
+  getHomeAnime,
+  searchAnime,
+  getCompleteAnime,
+  getOnGoingAnime,
+} from "./repo";
 
 export async function Home(_: Request, res: Response) {
   try {
@@ -20,6 +25,36 @@ export async function Search(req: Request, res: Response) {
         query,
       },
       data: buffer,
+    });
+  } catch (err) {
+    res.render("error", { message: err as string });
+  }
+}
+
+export async function Completed(req: Request, res: Response) {
+  try {
+    const page = parseInt((req.query.page as string) || "1");
+    const buffer = await getCompleteAnime(page);
+    let pages: number[] = [];
+    if (buffer.lastPage > 12) {
+      if (page <= 10 || page === buffer.lastPage) {
+        pages = Array.from({ length: 9 }, (_, i) => i + 2);
+      } else if (page < buffer.lastPage - 1) {
+        pages = Array.from({ length: 7 }, (_, i) => i + 2);
+      } else if (page == buffer.lastPage - 1) {
+        pages = Array.from({ length: 8 }, (_, i) => i + 2);
+      }
+    }else{
+      pages = Array.from({length: buffer.lastPage - 2}, (_, i) => i + 2);
+    }
+    res.render("completed", {
+      meta: {
+        page,
+        lastPage: buffer.lastPage,
+        beforeLast: buffer.lastPage - 1,
+        pages,
+      },
+      data: buffer.animeList,
     });
   } catch (err) {
     res.render("error", { message: err as string });
--- src/apps/anime/webRoute.ts
+++ src/apps/anime/webRoute.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { Home, Search } from "./webController";
+import { Home, Search, Completed } from "./webController";
 
 const route = Router();
 route.get("/", Home);
 route.get("/search", Search);
+route.get("/complete", Completed);
 
 export default route;
